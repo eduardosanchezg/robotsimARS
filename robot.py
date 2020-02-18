direction line calculation fixed

robot.py
@@ -31,7 +31,7 @@ class Robot:
         print(self.pos)
         print(self.velocity)
         if self.velocity[0] == self.velocity[1]:
-            self.pos += (self.velocity[0] + self.velocity[1]) * np.array([np.sin(self.pos[2]), np.cos(self.pos[2]), 0])
+            self.pos += (self.velocity[0] + self.velocity[1]) * np.array([np.cos(self.pos[2]), np.sin(self.pos[2]), 0])
             return
         R = (self.radius) * (np.sum(self.velocity)) / (np.diff(self.velocity))
         w = np.diff(self.velocity)/(2 * self.radius)